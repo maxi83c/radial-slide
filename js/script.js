c19c9c0340f997f531955c090072d0207c76a191
misterx83c <53312260+misterx83c@users.noreply.github.com>
2021-05-06 20:20:52 -0300

    get absolute path

--- js/script.js
+++ js/script.js
@@ -5,13 +5,21 @@ const container = document.getElementById('container');
 let animatedImage = 0;
 
 let index = 0;
+
+const getAbsolutePath = () => {
+  const loc = window.location;
+  const pathName = loc.pathname.substring(0, loc.pathname.lastIndexOf('/') + 1);
+  return loc.href.substring(0, loc.href.length - ((loc.pathname + loc.search + loc.hash).length - pathName.length));
+}
+
+
 const next = () => {
   const first = index;
   const second = index === 0 ? images.length - 1 : index - 1;
   index = second;
   frontImage.classList.remove('img__animate');
-  frontImage.setAttribute('src', `/assets/${images[first]}`);
-  backImage.setAttribute('src', `/assets/${images[second]}`);
+  frontImage.setAttribute('src', `${getAbsolutePath()}/assets/${images[first]}`);
+  backImage.setAttribute('src', `${getAbsolutePath()}/assets/${images[second]}`);
   setTimeout(() => {
     frontImage.classList.add('img__animate');
   }, 10);
@@ -19,6 +27,7 @@ const next = () => {
     next();
   }, 3000);
 }
+
 
 window.addEventListener('load', () =>{
   next();
