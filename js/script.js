e36fbd5cc26e9d739d6e0eb44e087399055d56b9
misterx83c <53312260+misterx83c@users.noreply.github.com>
2021-05-06 20:36:16 -0300

    update paths to github pages

--- js/script.js
+++ js/script.js
@@ -11,8 +11,8 @@ const next = () => {
   const second = index === 0 ? images.length - 1 : index - 1;
   index = second;
   frontImage.classList.remove('img__animate');
-  frontImage.setAttribute('src', `${window.location}assets/${images[first]}`);
-  backImage.setAttribute('src', `${window.location}assets/${images[second]}`);
+    frontImage.setAttribute('src', `https://raw.githubusercontent.com/maxi83c/radial-slide/master/assets/${images[first]}`);
+  backImage.setAttribute('src', `https://raw.githubusercontent.com/maxi83c/radial-slide/master/assets/${images[second]}`);
   setTimeout(() => {
     frontImage.classList.add('img__animate');
   }, 10);
