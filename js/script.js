426b17867d56aed988a75696f5b943bfb523d99f
misterx83c <53312260+misterx83c@users.noreply.github.com>
2021-05-06 20:30:29 -0300

    new fix

--- js/script.js
+++ js/script.js
@@ -8,8 +8,7 @@ let index = 0;
 
 const getAbsolutePath = () => {
   const loc = window.location;
-  const pathName = loc.pathname.substring(0, loc.pathname.lastIndexOf('/') + 1);
-  return loc.href.substring(0, loc.href.length - ((loc.pathname + loc.search + loc.hash).length - pathName.length));
+  return loc.pathname.substring(0, loc.pathname.lastIndexOf('/') + 1);
 }
 
 
